ef42d0dd76283d6171c3645d502fdd95e1ae03d1
shylin <shylinshine@163.com>
2020-04-29 09:52:46 +0800

    Add files via upload

---
+++ 01-第一个爬虫.py
@@ -0,0 +1,20 @@
+﻿from urllib.request import urlopen
+
+url = "http://www.baidu.com"
+# 发送请求
+response = urlopen(url)
+# 读取内容
+info = response.read()
+# if response.status_code==200:
+    # print("got it")
+# else:
+    # print("fail")
+# 打印内容
+print(info.decode())
+
+# 打印状态码
+# print(response.getcode())
+# # 打印真实url
+# print(response.geturl())
+# # 打印响应头
+# print(response.info())
